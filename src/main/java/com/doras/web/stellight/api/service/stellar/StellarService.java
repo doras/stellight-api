6068ba1595204fcd4c60f83c2b798007c310ecce
doras <shoon0113@gmail.com>
2023-02-21 00:13:19 +0900

    Feat: add read-only transaction to find method of stellar

--- src/main/java/com/doras/web/stellight/api/service/stellar/StellarService.java
+++ src/main/java/com/doras/web/stellight/api/service/stellar/StellarService.java
@@ -6,12 +6,14 @@ import com.doras.web.stellight.api.exception.StellarNotFoundException;
 import com.doras.web.stellight.api.web.dto.StellarsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class StellarService {
     private final StellarRepository stellarRepository;
 
+    @Transactional(readOnly = true)
     public StellarsResponseDto findById(Long id) {
         Stellar entity = stellarRepository.findById(id)
                 .orElseThrow(() -> new StellarNotFoundException(id));
