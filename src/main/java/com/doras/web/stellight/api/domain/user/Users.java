584ad20c97417fc8f4ea1c2f8a8b27025b8a1a69
doras <shoon0113@gmail.com>
2023-02-27 23:19:31 +0900

    Feat: add Users, Naver OAuth & config security

---
+++ src/main/java/com/doras/web/stellight/api/domain/user/Users.java
@@ -0,0 +1,55 @@
+package com.doras.web.stellight.api.domain.user;
+
+import com.doras.web.stellight.api.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * User entity.
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class Users extends BaseTimeEntity {
+    /**
+     * Primary key
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * (Required) email address
+     */
+    @Column(nullable = false)
+    private String email;
+
+    /**
+     * (Required) role of the user
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    /**
+     * Constructor of Users
+     * @param email values for {@link #email}
+     * @param role values for {@link #role}
+     */
+    @Builder
+    public Users(String email, Role role) {
+        this.email = email;
+        this.role = role;
+    }
+
+    /**
+     * Get key of {@link #role}
+     * @return key of role
+     */
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
