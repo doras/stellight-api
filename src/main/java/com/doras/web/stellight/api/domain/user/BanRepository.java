1ac57d37a7cf0ba0817b44ac670a933af0576140
SeungHoon Yun <44899209+doras@users.noreply.github.com>
2023-03-15 17:41:24 +0900

    Revert "version 1.0.0 (#20)"
    This reverts commit 3c76966c5316f5af663649ccf7c1e289e71e3066.

--- src/main/java/com/doras/web/stellight/api/domain/user/BanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.doras.web.stellight.api.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * JPA repository for {@link Ban}.
- */
-public interface BanRepository extends JpaRepository<Ban, Long> {
-}
